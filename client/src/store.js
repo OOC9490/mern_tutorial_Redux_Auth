b679cb3b9a7ee0fed140e1a8b597dca162779893
Lawrence Carreon <l.carreon123@gmail.com>
2020-04-06 18:17:59 +1000

    testing a fix to store.js on heroku deployment

--- client/src/store.js
+++ client/src/store.js
@@ -2,12 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {};
-
 const middleware = [thunk];
 
 // prepare the redux state manager
-const store = createStore( rootReducer, initialState, compose(
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore( rootReducer, composeEnhancers(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
